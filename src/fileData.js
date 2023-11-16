e1ea3a8b7f6c847a660c6425b6a02fc197fd985b
Arturo Quiroz <r2inlondon@gmail.com>
2023-11-16 18:11:29 +0000

    fileData file created

---
+++ src/fileData.js
@@ -0,0 +1,30 @@
+const storedFiles = [
+    {
+        id: 1,
+        name: "libros.xls",
+        filetype: "Excel file",
+        size: "4324521",
+        created_at: "2023-10-21T18:54:23.081Z",
+    },
+    {
+        id: 2,
+        name: "budged.xls",
+        filetype: "Excel file",
+        size: "232341",
+        created_at: "2023-10-27T13:50:50.687Z",
+    },
+    {
+        id: 3,
+        name: "application.pdf",
+        filetype: "PDF file",
+        size: "324521",
+        created_at: "2023-10-27T13:50:55.646Z",
+    },
+    {
+        id: 4,
+        name: "avatar.jpeg",
+        filetype: "Image file",
+        size: "2800521",
+        created_at: "2023-11-13T18:07:15.732Z",
+    },
+];
